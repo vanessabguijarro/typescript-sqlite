f3d080a435a40fc58c077c96469f4f6cd385a268
Israel <cursos.israelsp@gmail.com>
2025-04-01 19:51:31 +0200

    Iniciando

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Notas iniciais
+
+## Forma de executalo
+
+> `ts-node index` out `npx ts-node index`
+Con iso teremos de saída o seguinte:
+
+```json
+result: { col: 'test' }
+{ col: 'test' }
+```
+
+Partimos da base que temos instalado ***ts-node*** e ***typescript*** en modo global
---
+++ index.ts
@@ -0,0 +1,41 @@
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
+
+// you would have to import / invoke this in another file
+export async function openDb () {
+  return open<sqlite3.Database, sqlite3.Statement>({
+    filename: './proba.db',
+    driver: sqlite3.Database
+  })
+}
+
+async function lerDatos(){
+    const db = await openDb();
+    await db.exec('INSERT INTO tbl VALUES ("test")');
+    
+    const result = await db.get('SELECT col FROM tbl WHERE col = ?', 'test');
+    console.log('result:', result);
+    console.log(await db.get('SELECT col FROM tbl WHERE col = ?', 'test'));
+}
+lerDatos()
+/*
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+async function run() {
+    // sqlite3.Database, sqlite3.Statement es el valor por defecto si no se especifica un genérico explícito
+    const db = await open<sqlite3.Database, sqlite3.Statement>({
+        filename: './proba.db',
+        driver: sqlite3.Database,
+    });
+
+    //await db.exec('CREATE TABLE tbl (col TEXT)');
+    await db.exec('INSERT INTO tbl VALUES ("test")');
+    
+    const result = await db.get('SELECT col FROM tbl WHERE col = ?', 'test');
+    console.log('result:', result);
+    console.log(await db.get('SELECT col FROM tbl WHERE col = ?', 'test'));
+}
+
+run().catch(err => console.error(err));
+*/
